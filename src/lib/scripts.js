e22f33288a6af72e1e098bfb541d940dffce769d
antoniputra <akiddcode@gmail.com>
2016-05-03 15:51:40 +0700

    fix demo

--- src/lib/scripts.js
+++
@@ -1,5 +0,0 @@
-module.exports = (function() {
-	$("body").tooltip({
-		selector: '[data-toggle="tooltip"]'
-	});
-})
--- src/pages/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<script>
-	
-</script>
-
-<template>
-	<div class="alert alert-danger">
-		<p>Page that you're looking for is not available</p>
-	</div>
-</template>
